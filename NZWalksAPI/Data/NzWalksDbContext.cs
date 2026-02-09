b9b6b20b185d0754d3daf0b0e67a2a584df2d81c
ehsanexe <ehsanexe01@gmail.com>
2026-02-09 16:32:51 +0500

    first

---
+++ NZWalksAPI/Data/NzWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
